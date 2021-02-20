6e21b4776d93ee7a93c23d8cf3db4d8b82d66a10
Inoue <glabra@users.noreply.github.com>
2021-02-21 00:00:52 +0900

    fixc deprecated syntax, add required permissions

--- main.tf
+++ main.tf
@@ -24,8 +24,9 @@ data "aws_iam_policy_document" "policy" {
       "s3:ListBucketMultipartUploads",
       "s3:AbortMultipartUpload",
       "s3:ListMultipartUploadParts",
+      "s3:HeadBucket",
     ]
-    resources = flatten([for s in var.s3_bucket_arns : ["${s}", "${s}/*"]])
+    resources = flatten([for s in var.s3_bucket_arns : [s, "${s}/*"]])
   }
 }
 
