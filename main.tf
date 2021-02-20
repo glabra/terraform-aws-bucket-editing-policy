eaf56fce1c53e81f0514b8068b01a80363b7c61a
Inoue <glabra@users.noreply.github.com>
2021-02-21 00:35:29 +0900

    allow every object operation

--- main.tf
+++ main.tf
@@ -18,9 +18,7 @@ data "aws_iam_policy_document" "policy" {
   
   statement {
     actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:DeleteObject",
+      "s3:*Object",
       "s3:GetBucketLocation",
       "s3:ListBucketMultipartUploads",
       "s3:AbortMultipartUpload",
